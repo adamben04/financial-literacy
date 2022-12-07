164916c5363252a19d00876cf65fa44f8cd752c3
polaristardust <wintran987@gmail.com>
2022-12-06 17:32:44 -0800

    add audio to calculate button

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-button.addEventListener("hover",)
+let button = document.querySelector("button");
+button.addEventListener("click", playAudio);
+
+function playAudio(event){
+    var audio = new Audio("audio/mixkit-coins-sound-2003.mp3");
+    console.log(audio);
+    audio.play();
+};
